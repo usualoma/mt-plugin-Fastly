3d8e99d8d4328a06e4199bcd64e725f8025aed5f
Taku AMANO <taku@taaas.jp>
2015-12-15 05:38:56 +0900

    Implement more features.

--- plugins/Fastly/lib/MT/Plugin/Fastly.pm
+++ plugins/Fastly/lib/MT/Plugin/Fastly.pm
@@ -7,18 +7,24 @@ use utf8;
 use MT::Util;
 use HTTP::Request;
 
+use constant SURROGATE_KEY => 'mt-plugin-Fastly-dynamic';
+
+sub plugin {
+    MT->component('Fastly');
+}
+
 sub translate {
-    MT->component('Fastly')->translate(shift);
+    plugin()->translate(shift);
 }
 
 sub enabled {
     my ($blog) = @_;
-    MT->component('Fastly')->get_config_value('fastly_enabled', 'blog:' . $blog->id);
+    plugin()->get_config_value('fastly_enabled', 'blog:' . $blog->id);
 }
 
 sub cname {
     my ($blog) = @_;
-    MT->component('Fastly')->get_config_value('fastly_cname', 'blog:' . $blog->id);
+    plugin()->get_config_value('fastly_cname', 'blog:' . $blog->id);
 }
 
 sub cache_server_uris {
@@ -30,9 +36,21 @@ sub cache_server_uris {
     } split(/\s*,\s*/, cname($blog))];
 }
 
+sub surrogate_keys {
+    my ($blog) = @_;
+    my $keys_str = plugin()->get_config_value('fastly_surrogate_keys', 'blog:' . $blog->id);
+
+    [ grep { $_ } split(/\s+/sm, $keys_str) ];
+}
+
+sub service_id {
+    my ($blog) = @_;
+    plugin()->get_config_value('fastly_service_id', 'blog:' . $blog->id);
+}
+
 sub additional_headers {
     my ($blog) = @_;
-    my $text = MT->component('Fastly')->get_config_value('fastly_additional_headers', 'blog:' . $blog->id) || '';
+    my $text = plugin()->get_config_value('fastly_additional_headers', 'blog:' . $blog->id) || '';
     +{
         map {
             split /\s*:\s*/, $_;
@@ -42,14 +60,28 @@ sub additional_headers {
 
 sub wait_for_prview {
     my ($blog) = @_;
-    MT->component('Fastly')->get_config_value('fastly_wait_for_preview', 'blog:' . $blog->id);
+    plugin()->get_config_value('fastly_wait_for_preview', 'blog:' . $blog->id);
 }
 
 sub debug_mode {
-    MT->component('Fastly')->get_config_value('fastly_debug_mode');
+    plugin()->get_config_value('fastly_debug_mode');
 }
 
-sub send_purge_request {
+sub error_log {
+    MT->instance->log({
+        message => shift,
+        level   => MT->model('log')->ERROR,
+    });
+}
+
+sub debug_log {
+    MT->instance->log({
+        message => shift,
+        level   => MT->model('log')->DEBUG,
+    });
+}
+
+sub send_instant_purge_request {
     my ($host, $url, $headers) = @_;
 
     my $req = HTTP::Request->new(PURGE => $url);
@@ -59,21 +91,21 @@ sub send_purge_request {
     }
 
     if (debug_mode()) {
-        MT->instance->log("PURGE Host: $host URL: $url");
+        debug_log("PURGE Host: $host URL: $url");
     }
 
     my $ua  = MT->new_ua;
     my $res = $ua->request($req);
 
     unless ($res->is_success) {
-        MT->instance->log($res->status_line);
+        error_log($res->status_line);
     }
     elsif (debug_mode()) {
-        MT->instance->log($res->decoded_content);
+        debug_log($res->decoded_content);
     }
 }
 
-sub purge {
+sub instant_purge {
     my ($blog, $url) = @_;
 
     my $uri     = URI->new($url);
@@ -85,17 +117,60 @@ sub purge {
         $uri->scheme($u->scheme);
 
         my $uri_str = $uri->as_string;
-        send_purge_request($host, $uri_str, $headers);
+        send_instant_purge_request($host, $uri_str, $headers);
 
         my $stripped_uri_str = MT::Util::strip_index($uri_str, $blog);
-        send_purge_request($host, $stripped_uri_str, $headers) if $stripped_uri_str ne $uri_str;
+        send_instant_purge_request($host, $stripped_uri_str, $headers) if $stripped_uri_str ne $uri_str;
     }
 }
 
-sub add {
+sub send_purge_by_surrogate_key_request {
+    my ($service_id, $surrogate_key) = @_;
+
+    my $api_key = MT->config->FastlyAPIKey
+        or return 1;
+
+    my $req = HTTP::Request->new(
+        POST => "https://api.fastly.com/service/$service_id/purge/$surrogate_key"
+    );
+    $req->header('Fastly-Key' => $api_key);
+
+    if (debug_mode()) {
+        debug_log('PURGE Surrogate Key: ' . $surrogate_key);
+    }
+
+    my $ua  = MT->new_ua;
+    my $res = $ua->request($req);
+
+    unless ($res->is_success) {
+        error_log($res->status_line);
+    }
+    elsif (debug_mode()) {
+        debug_log($res->decoded_content);
+    }
+}
+
+sub purge_by_surrogate_key {
+    my ($blog) = @_;
+
+    my $service_id = service_id($blog)
+        or return 1;
+
+    for my $k (@{surrogate_keys($blog)}) {
+        send_purge_by_surrogate_key_request($service_id, $k);
+    }
+}
+
+sub add_url {
     my ($blog, $url) = @_;
     my $map = MT->request('FastlyPurgeURLs') || MT->request('FastlyPurgeURLs', {});
     $map->{$url} = {blog => $blog};
+}
+
+sub add_blog {
+    my ($blog) = @_;
+    my $map = MT->request('FastlyPurgeBlogs') || MT->request('FastlyPurgeBlogs', {});
+    $map->{$blog->id} = {blog => $blog};
 }
 
 sub build_file {
@@ -106,25 +181,69 @@ sub build_file {
 
     my $blog_site_url = $blog->site_url;
     $blog_site_url =~ s{/$}{};
-    add($blog, $blog_site_url . $fi->url);
+    add_url($blog, $blog_site_url . $fi->url);
 
+    1;
+}
+
+sub pre_remove_session {
+    my ($cb, $ses) = @_;
+    my $app = MT->instance
+        or return 1;
+
+    return 1 if $ses->kind ne 'TF';
+
+    my $blog = $app->can('blog') && $app->blog
+        or return 1;
+    my $file = $ses->name;
+    $file =~ s/@{[$blog->site_path]}//;
+    $file =~ s{ \A /+ }{}xms;
+
+    my $site_url = $blog->site_url;
+    $site_url =~ s{ / \z }{}xms;
+
+    add_url($blog, join(q{/}, $site_url, $file));
+}
+
+sub pre_remove_fileinfo {
+    my ($cb, $fi) = @_;
+
+    require MT::Blog;
+    my $blog = MT::Blog->load($fi->blog_id)
+        or return 1;
+
+    return 1 unless enabled($blog);
+
+    my $blog_site_url = $blog->site_url;
+    $blog_site_url =~ s{/$}{};
+    add_url($blog, $blog_site_url . $fi->url);
+}
+
+sub permission_filter_asset {
+    my ($cb, $app, $asset) = @_;
+    add_url_asset($asset);
     1;
 }
 
 sub cms_upload_file {
     my ($app, %params) = @_;
-    my ($asset) = @params{qw(asset)};
+    add_url_asset($params{asset});
+}
+
+sub add_url_asset {
+    my ($asset) = @_;
+
     my $blog = $asset->blog;
 
     return 1 unless enabled($blog);
 
-    add($blog, $asset->url);
+    add_url($blog, $asset->url);
 
     my $blog_site_path = $blog->site_path;
     my $blog_site_url  = $blog->site_url;
     for my $f (cached_files($asset)) {
         $f =~ s/\Q$blog_site_path\E/$blog_site_url/;
-        add($blog, $f);
+        add_url($blog, $f);
     }
 
     1;
@@ -160,22 +279,28 @@ sub post_run {
         ($mode eq 'preview_entry' || $mode eq 'preview_template')
         && enabled($app->blog)
     ) {
-        add($app->blog, $app->{redirect}) if $app->{redirect};
+        add_url($app->blog, $app->{redirect}) if $app->{redirect};
 
         my $body = $app->response_content();
         if (ref($body) && $body->isa('MT::Template')) {
             my $preview_url = $body->param->{preview_url};
-            add($app->blog, $preview_url) if $preview_url;
+            add_url($app->blog, $preview_url) if $preview_url;
         }
     }
 
     my $map = MT->request('FastlyPurgeURLs');
     if ($map) {
         for my $url (keys %$map) {
-            purge($map->{$url}{blog}, $url);
+            instant_purge($map->{$url}{blog}, $url);
         }
     }
 
+    my $blog_map = MT->request('FastlyPurgeBlogs');
+    if ($blog_map) {
+        for my $data (values %$blog_map) {
+            purge_by_surrogate_key($data->{blog});
+        }
+    }
 
     if (
         ($mode eq 'preview_entry' || $mode eq 'preview_template')
@@ -205,6 +330,54 @@ sub init_app {
     }
 }
 
+sub cms_init_app {
+    init_app();
+
+    {
+        no warnings 'redefine';
+        my $app = MT->instance;
+        *MT::App::CMS::send_http_header = sub {
+            MT::App::send_http_header(@_);
+            $app->set_header('Cache-Control' => 'private, no-cache');
+        };
+        \&MT::App::CMS::send_http_header;
+    }
+}
+
+sub search_init_app {
+    init_app();
+
+    {
+        no warnings 'redefine';
+        my $app = MT->instance;
+        *MT::App::Search::send_http_header = sub {
+            MT::App::send_http_header(@_);
+            $app->set_header('Surrogate-Key' => SURROGATE_KEY);
+        };
+        \&MT::App::Search::send_http_header;
+    }
+}
+
+sub data_api_init_app {
+    init_app();
+
+    {
+        no warnings 'redefine';
+        my $app  = MT->instance;
+        my $orig = \&MT::App::DataAPI::send_http_header;
+        *MT::App::DataAPI::send_http_header = sub {
+            $orig->(@_);
+
+            my $user = $app->authenticate;
+            if (!$user && !$user->is_anonymous) {
+                $app->set_header('Cache-Control' => 'private, no-cache');
+            }
+
+            $app->set_header('Surrogate-Key' => SURROGATE_KEY);
+        };
+    }
+}
+
 sub include_statement {
     my $orig = shift;
     my $blog = shift;
@@ -217,12 +390,28 @@ sub include_statement {
     my $site_url = $blog->site_url;
     $site_url =~ s{ / \z }{}xms;
 
-    add($blog, join(q{/}, $site_url, @path, $filename));
+    add_url($blog, join(q{/}, $site_url, @path, $filename));
 
     $site_url =~ s{ \A \w+ :// [^/]+ }{}xms;
     my $include = join q{/}, $site_url, @path, $filename;
 
     sprintf qq{<esi:include src="%s"/>}, MT::Util::encode_php($include, q{qq});
+}
+
+sub pre_save_object {
+    my ($cb, $obj) = @_;
+
+    return 1 if (ref($obj) =~ m/session|::log$|::revision$|touch|fileinfo/i);
+
+    my $blog
+        = $obj->isa('MT::Blog') ? $obj
+        : $obj->can('blog')     ? $obj->blog
+        :                         undef;
+    return 1 unless $blog;
+
+    return 1 unless enabled($blog);
+
+    add_blog($blog);
 }
 
 1;
